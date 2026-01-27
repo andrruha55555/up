a4d7b187c0d76ac4ee0687dbd131dc16b5763dcf
DikiYLexa <dubovan36@gmail.com>
2026-01-27 16:28:20 +0500

    Создан класс Classroom.Сыпачев

---
+++ Models/Classroom.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiUp.Model
+{
+    public class Classroom
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string short_name { get; set; }
+        public int? responsible_user_id { get; set; }
+        public int? temp_responsible_user_id { get; set; }
+    }
+}
